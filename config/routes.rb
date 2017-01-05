bda5cc1ae68d037b9c4765601b0841d2c9f81b34
Brad <bgreen3@uoregon.edu>
2017-01-05 14:03:48 -0700

    Add unstyled root page, controller, and route

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
+  root 'home#index'
+  
   resources :users
 
   get '/dashboard', to: 'dashboard#show'
-  
+
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
