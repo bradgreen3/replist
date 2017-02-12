928d76cbbd72fea7308974bd764f15080c1eb3f1
Brad <bgreen3@uoregon.edu>
2017-02-12 13:48:27 -0700

    Add AJAX and flash message for deleting a video

--- app/assets/javascripts/pieces-show.js
+++ app/assets/javascripts/pieces-show.js
@@ -6,7 +6,8 @@ function flashSuccess() {
   $('.pieces-show').prepend('<div class="alert alert-success alert-dismissable"> <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a> Your video has been removed from YouTube. </div>')
   $('.youtube-player').remove();
   $('.youtube-icons .btn').remove();
-  
+  var pathName = window.location.pathname
+  $('.col-md-6').append(`<form class="testytesty" method="get" action="${pathName}/youtube_uploads/new"><input class="btn btn-primary" type="submit" value="Upload Video"></form>`)
 }
 
 $('.pieces-show').ready(function() {
