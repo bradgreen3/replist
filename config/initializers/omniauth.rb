7ae6d2281151ed5bb3b5b0b797a00796ee303a34
Brad <bgreen3@uoregon.edu>
2017-03-12 19:03:53 -0600

    Fix scope issue in omniauth initializer

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :google_oauth2, ENV['youtube_id'], ENV['youtube_secret'], scope: 'userinfo.profile,youtube'
+  provider :google_oauth2, ENV['youtube_id'], ENV['youtube_secret'], scope: ['youtube', 'plus.me']
 end
--- config/initializers/yt.rb
+++ config/initializers/yt.rb
@@ -1,5 +1,3 @@
 Yt.configure do |config|
   config.api_key = ENV['youtube_api']
 end
-
-Yt.configuration.api_key = ENV['youtube_api']
