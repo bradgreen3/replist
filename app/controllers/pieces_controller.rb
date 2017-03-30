2ca9768cfa3beaf8f66698b398e03b8075f7cf3c
Brad <bgreen3@uoregon.edu>
2017-03-29 21:55:06 -0600

    Fix check_yt method to work for not logged in users

--- app/controllers/pieces_controller.rb
+++ app/controllers/pieces_controller.rb
@@ -59,8 +59,8 @@ class PiecesController < ApplicationController
 
   def check_yt
     piece = Piece.find(params[:id])
-    user = YoutubeUser.find_by(user_id: current_user)
-    unless piece.yt_uid == ""
+    user = YoutubeUser.find_by(user_id: params["user_id"].to_i)
+    if piece.yt_uid != "" && !piece.yt_uid != nil && user != nil
       piece.update_attributes(yt_uid: "", yt_link: "") if YoutubeService.new(piece.yt_uid, user.token).on_yt? == false
     end
   end
